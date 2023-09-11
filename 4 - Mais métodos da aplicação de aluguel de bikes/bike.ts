883f2e0197376eea0a303072e24253f23b149896
Alkhiat-L <128657996+Alkhiat-L@users.noreply.github.com>
2023-09-11 10:59:59 -0300

    Create bike.ts

---
+++ 4 - Mais métodos da aplicação de aluguel de bikes/bike.ts
@@ -0,0 +1,11 @@
+export class Bike {
+    constructor(
+        public name: string,
+        public type: string,
+        public body_size: number,
+        public max_load: number,
+        public description: string,
+        public plate: string, 
+        public id?: string
+    ) {}
+}
