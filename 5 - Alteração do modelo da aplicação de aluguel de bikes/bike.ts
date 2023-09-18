4e685ca8b60604dd23f06ad52914fa42747f41e2
Alkhiat-L <128657996+Alkhiat-L@users.noreply.github.com>
2023-09-18 11:10:36 -0300

    Update bike.ts

--- 5 - Alteração do modelo da aplicação de aluguel de bikes/bike.ts
+++ 5 - Alteração do modelo da aplicação de aluguel de bikes/bike.ts
@@ -1,13 +1,15 @@
 export class Bike {
-  public disponible: boolean = true
     constructor(
+        public plate: string,
         public name: string,
         public type: string,
         public body_size: number,
         public max_load: number,
-        public description: string,
-        public plate: string, 
         public rate: number,
+        public description: string,
+        public ratings: number,
+        public image_urls: string[],
+        public available: boolean = true,
         public id?: string
     ) {}
 }
