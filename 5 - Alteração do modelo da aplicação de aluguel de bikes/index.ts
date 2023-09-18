2f73cba85483e12f331cbabeb579bcefcd83b616
Alkhiat-L <128657996+Alkhiat-L@users.noreply.github.com>
2023-09-18 11:18:50 -0300

    Update index.ts

--- 5 - Alteração do modelo da aplicação de aluguel de bikes/index.ts
+++ 5 - Alteração do modelo da aplicação de aluguel de bikes/index.ts
@@ -1,41 +1,23 @@
-import { Rent } from "./rent";
+import { App } from "./app";
 import { Bike } from "./bike";
+import { Rent } from "./rent";
 import { User } from "./user";
-import { App } from "./app"
+import sinon from 'sinon'
 
+async function main() {
+    const clock = sinon.useFakeTimers();
+    const app = new App()
+    const user1 = new User('Jose', 'jose@mail.com', '1234')
+    await app.add_user(user1)
+    const bike = new Bike('0000-A01','caloi mountainbike', 'mountain bike',
+        1234, 1234, 100.0, 'My bike', 5, [])
+    app.add_bike(bike)
+    app.list_bikes()
+    app.add_rent(bike.plate, user1.email)
+    app.list_bikes()
+    clock.tick(1000 * 60 * 65)
+    console.log(app.return_bike(bike.plate, user1.email))
+    app.list_bikes()
+}
 
-
-const bike = new Bike('mountain bike', 'mountain', 123, 130, '', 'ABC-0001', 10)
-const bike2 = new Bike('road bike', 'road', 123, 130, '', 'ABC-0002', 20)
-const user = new User('Maria', 'maria@mail.com', '1234')
-const today = new Date()
-const twoDaysFromToday = new Date()
-twoDaysFromToday.setDate(twoDaysFromToday.getDate() + 2)
-const tomorrow = new Date()
-tomorrow.setDate(tomorrow.getDate() + 1)
-const sevenDaysFromToday = new Date()
-sevenDaysFromToday.setDate(sevenDaysFromToday.getDate() + 7)
-
-
-
-const user2 = new User('Maria Clara', 'maria2@mail.com', '3123')
-
-const app = new App()
-app.add_user(user)
-app.add_user(user2)
-console.log("USERS:")
-app.list_users()
-app.add_bike(bike)
-app.add_bike(bike2)
-console.log("BIKES:")
-app.list_bikes()
-console.log("RENTS:")
-app.add_rent('ABC-0001', 'maria@mail.com', today)
-app.list_rents()
-app.add_rent('ABC-0002', 'maria2@mail.com', today)
-app.list_rents()
-app.return_bike('ABC-0001', 'maria@mail.com')
-app.list_rents()
-app.return_bike('ABC-0002', 'maria2@mail.com')
-app.list_rents()
-app.authenticate('maria@mail.com', '1234')
+main()
